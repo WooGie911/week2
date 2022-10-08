3d6f15f25422efd9b960de4e9ed114601b000b4d
rlawodnr1109 <wodnr1109@naver.com>
2022-10-08 20:10:17 +0900

    n입력 받아서 증감하는 카운터 오류잡고 정상 작동 완료!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 // src/App.js
 
 import React from "react";
-import { useSelector } from "react-redux"; // import 해주세요.
-import { useDispatch } from "react-redux"; // import 해주세요.
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { minusNUM, plusNUM } from "./redux/modules/counter";
 import { useState } from "react";
 
 const App = () => {
   const [num, setNum] = useState(0);
   const dispatch = useDispatch(); // dispatch 생성
-  const gnumber = useSelector((state) => state.counter.number); // 추가해주세요.
+  const gnumber = useSelector((state) => state.counter.number);
+
+  const n = useSelector((s) => {
+    console.log(s);
+    return s;
+  });
 
   const onChangeHandler = (e) => {
     const { value } = e.target;
@@ -17,46 +22,81 @@ const App = () => {
     // event.target.value는 문자열 입니다.
     // 이것을 숫자형으로 형변환해주기 위해서 +를 붙여 주었습니다.
   };
-  // 콘솔로 onChangeHandler가 잘 연결되었는지 확인해봅니다.
-  // input에 값을 넣을 때마다 콘솔에 그 값이 찍히면 연결 성공!
+
   console.log(num);
-
-  const onClickAddNumberHandler = () => {
-    // 5. Action creator를 dispatch 해주고, 그때 Action creator의 인자에 number를 넣어줍니다.
-    dispatch(plusNUM(num));
-  };
-
-  const counterStore = useSelector((state) => state); // 추가해주세요.
-  console.log(counterStore); // 스토어를 조회해볼까요?
 
   return (
     <div>
       {gnumber}
       <input type="number" onChange={onChangeHandler} />
-      <button onClick={onClickAddNumberHandler}>더하기</button>
-      {/* <button
-        // 이벤트 핸들러 추가
+      <button
         onClick={() => {
-          // 마우스를 클릭했을 때 dispatch가 실행되고, ()안에 있는 액션객체가 리듀서로 전달된다.
           dispatch(plusNUM(num));
           // dispatch({ type: "PLUS_ONE" });
         }}
       >
         +
-      </button> */}
+      </button>
 
-      {/* <button
-        // 이벤트 핸들러 추가
+      <button
         onClick={() => {
-          // 마우스를 클릭했을 때 dispatch가 실행되고, ()안에 있는 액션객체가 리듀서로 전달된다.
           dispatch(minusNUM(num));
           // dispatch({ type: "MINUS_ONE" });
         }}
       >
         -
-      </button> */}
+      </button>
     </div>
   );
 };
 
 export default App;
+
+// import React from "react";
+// import { useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+
+// // 4. Action Creator를 import 합니다.
+// import { addNumber } from "./redux/modules/counter";
+// import { subNumber } from "./redux/modules/counter";
+
+// const App = () => {
+//   // 1. dispatch를 사용하기 위해 선언해줍니다.
+//   const dispatch = useDispatch();
+//   const [number, setNumber] = useState(0);
+//   const globalNumber = useSelector((state) => state.counter.number);
+//   console.log(globalNumber);
+
+//   const n = useSelector((s) => {
+//     console.log(s);
+//     return s;
+//   });
+
+//   const onChangeHandler = (evnet) => {
+//     const { value } = evnet.target;
+//     setNumber(+value);
+//   };
+
+//   // 2. 더하기 버튼을 눌렀을 때 실행할 이벤트핸들러를 만들어줍니다.
+//   const onClickAddNumberHandler = () => {
+//     // 5. Action creator를 dispatch 해주고, 그때 Action creator의 인자에 number를 넣어줍니다.
+//     dispatch(addNumber(number));
+//   };
+
+//   const onClickSubNumberHandler = () => {
+//     // 5. Action creator를 dispatch 해주고, 그때 Action creator의 인자에 number를 넣어줍니다.
+//     dispatch(subNumber(number));
+//   };
+
+//   return (
+//     <div>
+//       <div>{globalNumber}</div>
+//       <input type="number" onChange={onChangeHandler} />
+//       {/* 3. 더하기 버튼 이벤트핸들러를 연결해줍니다. */}
+//       <button onClick={onClickAddNumberHandler}>더하기</button>
+//       <button onClick={onClickSubNumberHandler}>빼기</button>
+//     </div>
+//   );
+// };
+
+// export default App;
