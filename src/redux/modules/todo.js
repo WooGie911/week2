e5392231fc74449553a8a71e0be2e30a408a0303
rlawodnr1109 <wodnr1109@naver.com>
2022-10-10 21:25:58 +0900

    todo 리듀서 나누고 store에 저장하는 방식 새로 확립하기로 함

---
+++ src/redux/modules/todo.js
@@ -0,0 +1,37 @@
+const INPUT_TEXT = "INPUT_TEXT";
+
+export const inputText = ({ payload }) => {
+  return {
+    type: INPUT_TEXT,
+    text: payload, //payload   key와 value 가 같으면 축약가능
+  };
+};
+
+// 초기 상태값
+const initialState = {
+  number: 0,
+  id: 0,
+  title: "",
+  body: "",
+};
+
+const todo = (state = initialState, action) => {
+  // console.log(action);
+  switch (action.type) {
+    case "INPUT_TEXT":
+      console.log(state);
+      return {
+        ...state,
+        inputText: action.text,
+        id: action.text,
+        title: action.text,
+        body: action.text,
+      };
+
+    default:
+      return state;
+  }
+};
+
+// 모듈파일에서는 리듀서를 export default 한다.
+export default todo;
